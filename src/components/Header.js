eb1247edeaa1ff1b146c88249347b8f5ae4c817a
Kian Sam <itskiansam@gmail.com>
2021-05-16 13:05:49 +0300

    ThirdCommit

---
+++ src/components/Header.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+const Header = (props) => {
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="!" />
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="/images/home-icon.svg" alt="Home" />
+          <span>HOME</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 5;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0;
+      white-space: nowrap;
+      position: relative;
+    }
+    &:before {
+      background-color: rgb(249, 249, 249);
+      border-radius: 0 0 4px 4px;
+    }
+  }
+
+  /* @media (max-width: 768px) {
+    display: none;
+  } */
+`;
+
+export default Header;
