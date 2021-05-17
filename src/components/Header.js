bba435d0f384599dd5fe4b4b0d6de02239218989
Kian Sam <itskiansam@gmail.com>
2021-05-17 21:28:11 +0300

    First commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,6 +41,7 @@ const Header = (props) => {
       <Logo>
         <img src="/images/logo.svg" alt="!" />
       </Logo>
+
       {!userName ? (
         <Login onClick={handleAuth}>Login</Login>
       ) : (
@@ -79,7 +80,6 @@ const Header = (props) => {
           </SignOut>
         </>
       )}
-      <Login onClick={handleAuth}>Login</Login>
     </Nav>
   );
 };
@@ -170,9 +170,9 @@ const NavMenu = styled.div`
     }
   }
 
-  /* @media (max-width: 768px) {
+  @media (max-width: 768px) {
     display: none;
-  } */
+  }
 `;
 
 const Login = styled.a`
@@ -191,6 +191,44 @@ const Login = styled.a`
   }
 `;
 
-const SignOut = styled;
+const UserImg = styled.img`
+  height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
 
 export default Header;
