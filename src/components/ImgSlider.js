92aba57a7579bda86dcf5b70423bdc822c2a3ec0
Kian Sam <itskiansam@gmail.com>
2021-05-21 18:05:40 +0300

    Slider Import

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const ImgSlider = (props) => {
   return <div>Img slider</div>;
