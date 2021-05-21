5fe2572c6918694eae8c6eb17c9a623cf7066c82
Kian Sam <itskiansam@gmail.com>
2021-05-21 20:36:45 +0300

    Carousel Complete

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -41,4 +41,53 @@ const ImgSlider = (props) => {
   );
 };
 
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
+  }
+`;
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  a {
+    border-radius: 4px;
+  }
+`;
+
 export default ImgSlider;
