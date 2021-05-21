1504a445d0f589b492b96ca3202be23af186bffe
Kian Sam <itskiansam@gmail.com>
2021-05-21 18:21:59 +0300

    Carousel-components

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -4,7 +4,41 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 const ImgSlider = (props) => {
-  return <div>Img slider</div>;
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-badging.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-scale.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-badag.jpg" alt="" />
+        </a>
+      </Wrap>
+
+      <Wrap>
+        <a href="">
+          <img src="/images/slider-scales.jpg" alt="" />
+        </a>
+      </Wrap>
+    </Carousel>
+  );
 };
 
 export default ImgSlider;
