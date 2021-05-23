9e1c647aa543e0876a178d66ed29f48502441474
Kian Sam <itskiansam@gmail.com>
2021-05-23 17:23:25 +0300

    Viewers/wrap-img

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -56,6 +56,26 @@ const Container = styled.div`
 const Wrap = styled.div`
   padding-top: 56.25%;
   border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+  }
 `;
 
 export default Viewers;
