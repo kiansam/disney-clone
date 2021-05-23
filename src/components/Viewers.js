51eab002f4122c40592a671c929ff35b03724032
Kian Sam <itskiansam@gmail.com>
2021-05-23 16:19:47 +0300

    Viewers V/I

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,7 +1,47 @@
 import styled from "styled-components";
 
 const Viewers = (props) => {
-  return <div>Viewers</div>;
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </Wrap>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+
+const Wrap = styled.div``;
 
 export default Viewers;
