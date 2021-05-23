f2482586e440e8c83a2ea325d02b80cbf59fd916
Kian Sam <itskiansam@gmail.com>
2021-05-23 16:02:08 +0300

    ImgSlider Complete

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -89,6 +89,21 @@ const Wrap = styled.div`
     border-radius: 4px;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    &:hover {
+      padding: 0;
+      border: 4px solid rgba(249, 249, 249, 0.8);
+      transition-duration: 300ms;
+    }
   }
 `;
 
