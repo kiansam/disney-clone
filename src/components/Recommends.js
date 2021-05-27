e6c7957b50786e23134fbb81a1bde63b18bd1c14
Kian Sam <itskiansam@gmail.com>
2021-05-27 23:55:48 +0300

    Recommends.js Content

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -17,6 +17,11 @@ const Content = styled.div`
   display: grid;
   grid-gap: 25px;
   gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
 `;
 
 export default Recommends;
