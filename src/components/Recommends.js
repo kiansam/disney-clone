7c03ba095e5625fc3b36aca8cd16dbd1d5277325
Kian Sam <itskiansam@gmail.com>
2021-05-28 21:04:22 +0300

    Recommends.js Wrap-complete

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -30,9 +30,35 @@ const Content = styled.div`
 `;
 
 const Wrap = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
 
-`
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+  }
 
-
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
 
 export default Recommends;
