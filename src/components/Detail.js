fca3172b7a224e75654dd60715ec8db3bbe65927
Kian Sam <itskiansam@gmail.com>
2021-05-30 21:29:30 +0300

    Detail.js

---
+++ src/components/Detail.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Detail = (props) => {
+  return <Container>Detail</Container>;
+};
+
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh-250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+`;
+
+
+
+export default Detail;
