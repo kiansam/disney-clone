5992c154e1521cac3cba49b3a738e1c0a7fa929f
Kian Sam <itskiansam@gmail.com>
2021-05-30 21:37:41 +0300

    Detail.js/Background

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 const Detail = (props) => {
-  return <Container>Detail</Container>;
+  return (
+    <Container>
+      <Background></Background>
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -13,6 +17,22 @@ const Container = styled.div`
   padding: 0 calc(3.5vw + 5px);
 `;
 
+const Background = styled.div`
+  left: 0;
+  opacity: 0.8;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: -1;
 
+  img {
+    width: 100vw;
+    height: 100vh;
+
+    @media (max-width: 768px) {
+      width: initial;
+    }
+  }
+`;
 
 export default Detail;
